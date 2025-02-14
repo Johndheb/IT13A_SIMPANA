6fd396c1ab4926b29898d9119983fe0f8b219745
misspatriana <simpanajohndheb@gmail.com>
2025-02-14 11:20:57 +0800

    Java File
    Added HelloWorld class

--- IT13A_SIMPANA/src/Prelim/Act1_Arithmetic_Operators.java
+++ IT13A_SIMPANA/src/Prelim/Act1_Arithmetic_Operators.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Prelim;
 
-/**
- *
- * @author Administrator
- */
+
 public class Act1_Arithmetic_Operators {
-    
+     public static void main(String[] org){
+             
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3; 
+        
+        
+        
+        int simpana1 = numb1 * numb3 + numb2;
+        int depends1 = numb1 - numb3;
+        int simpana2 = depends1 % numb2;
+        int depends2 = numb1 + numb3 + numb2;
+        int simpana3 = depends2 / numb4;
+        int depends3 = numb3 * numb3;
+        int simpana4 = numb1 * numb2 - depends3;
+        
+          System.out.println("10 * 4 + 6 ="+ simpana1);
+         System.out.println("(10-4) % 6 = "+ simpana2);
+            System.out.println("(10 + 4 + 6)/3 = "+ simpana3);
+               System.out.println("10 * 6 -(4 * 4 ) = "+ simpana4);
+               
+     }
 }
